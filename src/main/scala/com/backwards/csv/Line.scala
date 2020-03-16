80d2764916441a1d8656cdf6827920b6f194656e
David Ainslie <dainslie@gmail.com>
2020-03-16 15:48:58 +0000

    Removed code duplication

--- src/main/scala/com/backwards/csv/Line.scala
+++ src/main/scala/com/backwards/csv/Line.scala
@@ -15,10 +15,11 @@ final case class Line(accumulatedChars: List[String] = Nil, line: String = "")
 object Line {
   def conflate(csvConfig: CsvConfig)(line: Line, chars: String): Line = {
     val accumulatedChars = line.accumulatedChars :+ chars
+    val contiguousAccumulatedChars = accumulatedChars.mkString
 
-    if (accumulatedChars.mkString.count(_.toString == csvConfig.quote) % 2 == 0 &&
-        accumulatedChars.mkString.endsWith(csvConfig.lineDelimiter)) {
-      Line(Nil, accumulatedChars.mkString.stripSuffix(csvConfig.lineDelimiter) + lineSeparator)
+    if (contiguousAccumulatedChars.count(_.toString == csvConfig.quote) % 2 == 0 &&
+        contiguousAccumulatedChars.endsWith(csvConfig.lineDelimiter)) {
+      Line(Nil, contiguousAccumulatedChars.stripSuffix(csvConfig.lineDelimiter) + lineSeparator)
     } else {
       Line(accumulatedChars)
     }
